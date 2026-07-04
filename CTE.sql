594e9ed45e0e4fac59bfc5bfe755260687a12cc5
abharna27 <abharnamishra@gmail.com>
2026-07-04 09:53:17 +0530

    common table expression

---
+++ CTE.sql
@@ -0,0 +1,31 @@
+
+
+
+
+
+with cte_example as
+(
+SELECT  gender, avg(salary), max(salary),min(salary),count(salary)
+FROM parks_and_recreation.employee_demographics as dem
+join employee_salary as sal 
+                      on dem. employee_id = sal.employee_id
+group by gender)
+select*
+from cte_example;
+
+--- joining two subqueries---
+with cte_example as 
+(
+select employee_id , gender, birth_date 
+from employee_demographics
+where birth_date > '1985-01-01'),
+cte_example2 as
+(
+select employee_id , salary 
+from employee_salary
+where salary > 50000
+)
+select *
+from cte_example
+join cte_example2
+       on cte_example.employee_id = cte_example2.employee_id ;
